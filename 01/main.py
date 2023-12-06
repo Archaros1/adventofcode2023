a888878c66f9f6e6ccdb5492984085ae2b4484d1
Arthur JUNG <arthurjung2@hotmail.fr>
2023-12-06 22:31:38 +0100

    update day 1 part 2

--- 01/main.py
+++ 01/main.py
@@ -15,8 +15,8 @@ numbers = [
   'eight',
   'nine',
 ]
- 
-# ex1
+
+# Part 1
 sum = 0
 for line in Lines:
   finds = []
@@ -29,29 +29,27 @@ for line in Lines:
 
 print(sum)
 
-# ex2
+# Part 2
 sum = 0
 for line in Lines:
   finds = []
   pattern = '([0-9]|' + '|'.join(numbers) + ')'
-  output = re.search(pattern, line)
+  output = re.findall(pattern, line)
   finds.append(output[0])
-  pattern = '([0-9]|' + '|'.join([number[::-1] for number in numbers]) + ')'
-  output = re.search(pattern, line[::-1])
-  print(pattern)
+  pattern = '^.*([0-9]|' + '|'.join(numbers) + ')'
+  output = re.findall(pattern, line)
   finds.append(output[0])
+
   if len(finds[0]) > 1:
     finds[0] = numbers.index(finds[0])
   else:
     finds[0] = int(finds[0])
 
   if len(finds[1]) > 1:
-    finds[1] = [number[::-1] for number in numbers].index(finds[1])
+    finds[1] = numbers.index(finds[1])
   else:
     finds[1] = int(finds[1])
   
   sum += (finds[0] * 10) + finds[1]
 
 print(sum)
-
-# This is definitely not opti, but it was fun to make.
