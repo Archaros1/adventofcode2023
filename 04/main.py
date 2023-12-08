08d310f2e301bee79726320b4d2d8b6e7ff283cd
Arthur JUNG <arthurjung2@hotmail.fr>
2023-12-08 15:46:04 +0100

    day 4 part 2

--- 04/main.py
+++ 04/main.py
@@ -28,3 +28,35 @@ for lineKey, line in enumerate(lines):
   total += cardWorth
 
 print(total)
+
+
+# part 2
+print('Part 2')
+
+cardCount = []
+for i in range(0, len(lines)):
+  cardCount.append(1)
+
+for lineKey, line in enumerate(lines):
+  if cardCount[lineKey] > 0:
+    line = line.replace("\n", '')
+    numbers = line.split(': ')[1]
+    numbers = numbers.split(' | ')
+    winningNumbers = numbers[0]
+    myNumbers = numbers[1]
+    cardNumber = lineKey + 1
+    newCardsCount = 0
+    winningNumbers = re.findall('([0-9]{1,2})', winningNumbers)
+    myNumbers = re.findall('([0-9]{1,2})', myNumbers)
+
+    for number in myNumbers:
+      if number in winningNumbers:
+        newCardsCount += 1
+
+    for i in range(0, cardCount[lineKey]):
+      for j in range(1, newCardsCount + 1):
+        if lineKey + j < len(lines):
+          cardCount[lineKey + j] += 1
+
+# print(cardCount)
+print(sum(cardCount))
